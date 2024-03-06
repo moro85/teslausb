e86e710fcc204797d9dd4e8b6e0d034a0398fc58
Mor Omer <mor.omer85@gmail.com>
2024-03-06 23:19:11 +0200

    Update script reference

--- run/immich_archive/archive-clips.sh
+++ run/immich_archive/archive-clips.sh
@@ -2,7 +2,7 @@
 
 while [ -n "${1+x}" ]
 do
-  echo "$2" "$1" "$LOG_FILE"
-  ./upload.py "$2" "$1" >> "$LOG_FILE" 2>&1
+  echo "$2" "$1" >> "$LOG_FILE"
+  /root/bin/upload.py "$2" "$1" >> "$LOG_FILE" 2>&1
   shift 2
 done
