8430cc86ad393bca2109876b1d9ce003659d4562
Mor Omer <mor.omer85@gmail.com>
2024-03-06 19:41:31 +0200

    Install nodejs v20

--- setup/pi/configure.sh
+++ setup/pi/configure.sh
@@ -91,7 +91,12 @@ function check_immich {
     then
         echo "node is installed, skipping..."
     else
-  log_progress "installing nodejs"
+      log_progress "installing nodejs"
+      apt install -y ca-certificates curl gnupg
+      curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
+      NODE_MAJOR=20
+      echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+      apt update
       apt install -y nodejs
     fi
   if which npm > /dev/null
